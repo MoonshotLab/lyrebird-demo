4568de03940de9d0451e4dab4fa07b3c7b9bd0ba
Preston Richey <prichey@barkleyus.com>
2017-11-09 17:28:31 -0600

    Refactor generate route

--- routes/generate.js
+++ routes/generate.js
@@ -95,60 +95,21 @@ function generateLyrebirdUrlFromText(inputText) {
   });
 }
 
-function setupGenerateRoute() {
-  router.post('/', (req, res) => {
-    if (!!req.body && !!req.body.text && req.body.text.length > 0) {
-      const phrase = req.body.text;
-      generateLyrebirdUrlFromText(phrase)
-        .then(url => {
-          res.status(200).send(url);
-        })
-        .catch(e => {
-          console.log(e);
-          res.sendStatus(500);
-        });
-    } else {
-      res.sendStatus(500);
-    }
-  });
-}
-
-function setupProcessCloseEvents(browser) {
-  process.stdin.resume(); //so the program will not close instantly
-
-  async function exitHandler(options, err) {
-    if (options.cleanup) console.log('clean');
-    if (err) console.log(err.stack);
-
-    if (options.exit) {
-      try {
-        await browser.close();
-      } catch (e) {}
-
-      process.exit();
-      return Promise.resolve();
-    }
+router.post('/', (req, res) => {
+  if (!!req.body && !!req.body.text && req.body.text.length > 0) {
+    const phrase = req.body.text;
+    generateLyrebirdUrlFromText(phrase)
+      .then(url => {
+        res.status(200).send(url);
+      })
+      .catch(e => {
+        console.log(e);
+        res.sendStatus(500);
+      });
+  } else {
+    console.log('invalid request');
+    res.sendStatus(500);
   }
-
-  //do something when app is closing
-  process.on('exit', exitHandler.bind(null, { cleanup: true }));
-
-  //catches ctrl+c event
-  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-
-  // catches "kill pid" (for example: nodemon restart)
-  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
-  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
-
-  //catches uncaught exceptions
-  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
-}
-
-function run() {
-  setupGenerateRoute();
-  // logInToLyrebird();
-}
-
-run();
+});
 
 module.exports = router;
