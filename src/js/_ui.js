52ab9dd30e2f6e003d23efbc8c1bf1a235f5a015
Preston Richey <prichey@barkleyus.com>
2017-12-12 15:43:14 -0600

    Add affectiva for face tracking and screensaver

---
+++ src/js/_ui.js
@@ -0,0 +1,120 @@
+const NProgress = require('nprogress');
+const Promise = require('bluebird');
+
+const screensaver = require('./_screensaver');
+const video = require('./_video');
+
+const $form = $('#form');
+const $input = $('#input');
+const $submitButton = $('#submit');
+const $historySection = $('#recording-history');
+const $historyList = $('#history-list');
+
+const $cameraRoot = $('#camera-root');
+
+let screensaverTimeout = null;
+let screensaverTimeoutLength = 5 * 1000; // ms
+
+function addUtteranceToHistory(utterance) {
+  $historySection.show();
+
+  $historyList.prepend(
+    `<li><a href="/recordings/${utterance.id}" target="_blank">"${
+      utterance.text
+    }"</a></li>`
+  );
+}
+
+function playFromUrl(url) {
+  const sound = new Audio(url);
+  sound.play();
+}
+
+function setupInputChange() {
+  $input.on('keyup', function(e) {
+    const $this = $(this);
+    if ($this.val().length > 0) {
+      $submitButton.prop('disabled', false);
+    } else {
+      $submitButton.prop('disabled', true);
+    }
+  });
+}
+
+function setupFormSubmit() {
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    const text = $input.val();
+
+    if (!!text && text.length > 0) {
+      NProgress.start();
+      $.ajax({
+        type: 'POST',
+        url: '/generate',
+        data: {
+          text: text
+        },
+        timeout: 15 * 1000
+      })
+        .then(res => {
+          NProgress.done();
+
+          playFromUrl(res.audio_file);
+          $input.val('');
+
+          addUtteranceToHistory(res);
+        })
+        .catch(e => {
+          NProgress.done();
+          console.log('e', e);
+        });
+    }
+  });
+}
+
+// prevent screensaver on every user action
+function setupActivityWake() {
+  $(window).on('keydown mousemove mousedown', function(e) {
+    keepAlive();
+  });
+}
+
+// stave off screensaver
+function keepAlive() {
+  console.log('keepalive');
+  if (screensaver.isActivated()) {
+    wakeUp();
+  }
+
+  clearTimeout(screensaverTimeout);
+  screensaverTimeout = setTimeout(goToSleep, screensaverTimeoutLength);
+}
+
+// start screensaver, reset everything
+function goToSleep() {
+  screensaver.start();
+}
+
+function wakeUp() {
+  screensaver.stop();
+}
+
+export function asyncInit() {
+  return new Promise((resolve, reject) => {
+    try {
+      setupInputChange();
+      setupFormSubmit();
+      setupActivityWake();
+      video
+        .asyncSetupCamera($cameraRoot)
+        .then(video.startWatching)
+        .then(keepAlive)
+        .then(resolve);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+exports.keepAlive = keepAlive;
+exports.asyncInit = asyncInit;
--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,15 @@
-const NProgress = require('nprogress');
-
-const $form = $('#form');
-const $input = $('#input');
-const $submitButton = $('#submit');
-const $historySection = $('#recording-history');
-const $historyList = $('#history-list');
-
-function addUtteranceToHistory(utterance) {
-  $historySection.show();
-
-  $historyList.prepend(
-    `<li><a href="/recordings/${utterance.id}" target="_blank">"${
-      utterance.text
-    }"</a></li>`
-  );
-}
-
-function playFromUrl(url) {
-  const sound = new Audio(url);
-  sound.play();
-}
-
-function setupInputChange() {
-  $input.on('keyup', function(e) {
-    const $this = $(this);
-    if ($this.val().length > 0) {
-      $submitButton.prop('disabled', false);
-    } else {
-      $submitButton.prop('disabled', true);
-    }
-  });
-}
-
-function setupFormSubmit() {
-  $form.on('submit', function(e) {
-    e.preventDefault();
-    const text = $input.val();
-
-    if (!!text && text.length > 0) {
-      NProgress.start();
-      $.ajax({
-        type: 'POST',
-        url: '/generate',
-        data: {
-          text: text
-        },
-        timeout: 15 * 1000
-      })
-        .then(res => {
-          NProgress.done();
-
-          playFromUrl(res.audio_file);
-          $input.val('');
-
-          addUtteranceToHistory(res);
-        })
-        .catch(e => {
-          NProgress.done();
-          console.log('e', e);
-        });
-    }
-  });
-}
+const ui = require('./_ui');
+const video = require('./_video');
 
 function run() {
-  setupInputChange();
-  setupFormSubmit();
+  ui
+    .asyncInit()
+    .then(() => {
+      console.log('done!');
+    })
+    .catch(e => {
+      console.log('error initting', e);
+    });
 }
 
 $(window).on('load', function() {
